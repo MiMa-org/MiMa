be7a76874373428ec97607f3f12b8aa60d48bf50
Mauro T Barone <mautot.barone@gmail.com>
2022-09-14 15:59:30 +0200

    login signup input errors check

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,100 @@
-const { Router } = require('express');
-const router = new Router();
+// const { Router } = require('express');
+// const router = new Router();
+//  
+// router.get('/signup', (req, res) => res.render('auth/signup'));
+//  
+// module.exports = router;
 
 
- 
-router.get('/signup', (req, res) => res.render('auth/signup'));
- 
-// POST route ==> to process form data
- 
+
+const router = require("express").Router();
+// const User = require("../models/User");
+const bcryptjs = require("bcryptjs");
+// 
+router.get("/signup", (req, res, next) => {
+ res.render("signup");
+});
+// 
+router.post("/signup", (req, res, next) => {
+  const { email, firstName, lastName, password, checkPassword } = req.body;
+  if (password.length < 4) {
+    res.render("signup", {
+      errorMessage: "Your password needs to be min 4 chars",
+    });
+    return;
+  }
+  if (email.length === 0) {
+    res.render("signup", { errorMessage: "Your email cannot be empty" });
+    return;
+  }
+  if (password !== checkPassword) {
+    res.render("signup", { errorMessage: "Your passwords are not match!" });
+    return;
+  }
+
+  User.findOne({ email: email }).then((userFromDB) => {
+    if (userFromDB !== null) {
+      res.render("signup", { errorMessage: "Email is already taken" });
+    } else {
+      const salt = bcryptjs.genSaltSync();
+      const hash = bcryptjs.hashSync(password, salt);
+      User.create({
+        email,
+        firstName,
+        lastName,
+        passwordHash: hash,
+      })
+        .then((createdUser) => {
+          res.redirect("/login");
+        })
+        .catch((err) => next(err));
+    }
+  });
+});
+
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.render("login", {
+      errorMessage: "Please enter both, email and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.render("login", {
+          errorMessage: "Email is not registered. Try with other email.",
+        });
+        return;
+      } else if (!user.city && bcryptjs.compareSync(password, user.passwordHash)) {
+        req.session.currentUser = user;
+        res.redirect("/add-profile");
+      }
+      else if (user.city && bcryptjs.compareSync(password, user.passwordHash)) {
+        req.session.currentUser = user;
+        res.redirect("/all-rooms");
+      }
+      else {
+        res.render("login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+router.get("/venue/index", (req, res) => {
+  res.render("venue/index");
+});
+
 module.exports = router;
+// 
+// 
+// 
+// 
