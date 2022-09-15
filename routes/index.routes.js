cf8d2379368e8016e8a3f86e7a73963cdbae3341
Mauro T Barone <mautot.barone@gmail.com>
2022-09-15 15:33:20 +0200

    session auth

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,7 +7,8 @@ router.get('/', (req, res) => {
   Event.find()
     .then(eventsFromDB => {
       console.log("events: ", eventsFromDB)
-      res.render('index.hbs', { events: eventsFromDB });
+      console.log(req.session.currentUser)
+      res.render('index', { events: eventsFromDB, userInSession: req.session.currentUser});
     })
     .catch(err => console.log(`Error while getting the events from the DB: ${err}`));
 });
