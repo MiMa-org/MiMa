14225f681d6e7e1a30435af58cfdc6e2d0569281
Miriam Poletti <polettimiriam@gmail.com>
2022-09-15 10:21:33 +0200

    added page select

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,10 @@ const router = new Router();
 
  
 router.get('/signup', (req, res) => res.render('auth/signup'));
+
+router.get('/select', (req, res) => res.render('auth/select'));
+
+
  
 // POST route ==> to process form data
  
