326dc9b427297768a83ed63492c6ba6a76c2cb55
Mauro T Barone <mautot.barone@gmail.com>
2022-09-15 11:16:19 +0200

    1 user access

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,81 @@
 const { Router } = require('express');
 const router = new Router();
 
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+// controllare se vogliamo mettere altro al posto di User
+const User = require('../models/User.model');
+
 
  
 router.get('/signup', (req, res) => res.render('auth/signup'));
+router.get('/login', (req, res) => res.render('auth/login'));
+router.get('/select', (req, res) => res.render('auth/select'));
  
-// POST route ==> to process form data
+//               LOGIN
+
+// POST login route ==> to process form data
+router.post('/login', (req, res, next) => {
+    const { email, password } = req.body;
+   
+    if (email === '' || password === '') {
+      res.render('auth/login', {
+        errorMessage: 'Please enter both, email and password to login.'
+      });
+      return;
+    }
+   
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+          return;
+        } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        //   res.render('users/user-profile', { user });
+        res.render('index', {user});
+        } else {
+          res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        }
+      })
+      .catch(error => next(error));
+  });
+ 
+
+//              SIGN UP
+
+// POST signup route ==> to process form data
+router.post('/signup', (req, res, next) => {
+  // console.log("The form data: ", req.body);
+ 
+  const { username, email, password } = req.body;
+ 
+  bcryptjs
+  .genSalt(saltRounds)
+  .then(salt => bcryptjs.hash(password, salt))
+  .then(hashedPassword => {
+    return User.create({
+      // username: username
+      username,
+      email,
+      // passwordHash => this is the key from the User model
+      //     ^
+      //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+      passwordHash: hashedPassword
+    });
+  })
+  .then(userFromDB => {
+    // console.log('Newly created user is: ', userFromDB);
+    res.redirect('/userProfile');
+   
+
+  })
+  .catch(error => next(error));
+});
+
+// per adesso l'utente registrato va solo a userProfile, pagina di prova
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+
  
 module.exports = router;
